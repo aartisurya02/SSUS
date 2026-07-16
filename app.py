fbf2f8c59e07f412859e46fc6e6f2abd0154a15e
aartisurya02 <aartisurya70@gmail.com>
2026-07-16 15:45:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="SSUS",
+    layout="wide"
+)
+
+st.title("Smart Student Utility System")
+st.write(
+    " Students managment and Utilities"
+)           
---
+++ database/mongodb.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+try:
+    MONGO_URI = "mongodb+srv://aartigsp25_db_user:Aarti9340@cluster0.cxq6s8d.mongodb.net/?appName=Cluster0"
+
+    client = MongoClient(MONGO_URI)
+
+    client.andmin.command("ping")
+    
+    db = client["ssus"]
+    students_collection = db["students"]
+    marks_collection = db["marks"]
+    attendance_collection = db["attendance"]
+    bmi_collection = db["bmi_report"]
+
+    print("MongoDB connected Successfully!")
+except Exception as e:
+    print("MongoDB error:",e)
